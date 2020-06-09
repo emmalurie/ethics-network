c30332ca921fafce36652798d8599cf642215fb3
emmalurie <emmawlurie@gmail.com>
2020-06-08 21:38:59 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  8 16:30:51 2020
+
+@author: emma
+"""
+import plotly.graph_objects as go
+#import pandas as pd
+# -*- coding: utf-8 -*-
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from data import createGraph
+import pandas as pd
+g_df = pd.read_excel("google_links.xlsx")
+m_df = pd.read_excel("microsoft_links.xlsx")
+
+g_fig = createGraph(g_df, "Google")
+m_fig = createGraph(m_df, "Microsoft")
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    dcc.Tabs([
+        dcc.Tab(label='Google', children=[
+            dcc.Graph(
+                figure=g_fig
+            )
+        ]),
+        dcc.Tab(label='Microsoft', children=[
+            dcc.Graph(
+                figure=m_fig
+            )
+        ])
+    ])
+])
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
---
+++ data.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  8 16:30:51 2020
+
+@author: emma
+"""
+import networkx as nx
+import plotly.graph_objects as go
+
+
+def createGraph(df, title): 
+    traceRecode = []
+
+    G = nx.from_pandas_edgelist(df, 'source', 'destination')
+
+    pos = nx.spring_layout(G)
+    
+    #nodes
+    for node in G.nodes:
+        G.nodes[node]['pos'] = list(pos[node])
+        
+        
+    node_trace = go.Scatter(x=[], y=[], hovertext=[], text=[], mode='markers+text', textposition="bottom center",
+                        hoverinfo="text", marker={})
+
+    color_map = [] 
+    size = [] 
+    for node in G.nodes():
+        x, y = G.nodes[node]['pos']
+        hovertext = node
+        text = ""
+        node_trace['x'] += tuple([x])
+        node_trace['y'] += tuple([y])
+        node_trace['hovertext'] += tuple([hovertext])
+        node_trace['text'] += tuple([text])
+        
+        try:
+            if node in df.source.values: 
+                color_map.append('green')
+                size.append(25)
+            elif df.loc[df.destination == node, 'link_type'].iloc[0] == 'internal':
+                color_map.append('seagreen')
+                size.append(15)
+            else: 
+                color_map.append('blue')
+                size.append(15)
+        except IndexError:
+            color_map.append('purple')
+            size.append(15)
+    
+    node_trace['marker']['color'] = color_map
+    node_trace['marker']['size'] = size
+    
+    traceRecode.append(node_trace)
+        
+    #Edges
+    for edge in G.edges:
+        x0, y0 = G.nodes[edge[0]]['pos']
+        x1, y1 = G.nodes[edge[1]]['pos']
+        trace = go.Scatter(x=tuple([x0, x1, None]), y=tuple([y0, y1, None]),
+                           mode='lines',
+                           line={'width': 0.5},
+                           marker=dict(color='black'),
+                           line_shape='spline',
+                           opacity=0.5)
+        traceRecode.append(trace)
+    ### create figure dictionary 
+    figure = {
+    "data": traceRecode,
+    "layout": go.Layout(title=title, showlegend=False, hovermode='closest',
+                        margin={'b': 40, 'l': 40, 'r': 40, 't': 40},
+                        xaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
+                        yaxis={'showgrid': False, 'zeroline': False, 'showticklabels': False},
+                        height=600)}
+    return figure
